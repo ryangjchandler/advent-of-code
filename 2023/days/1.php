ea7f05dd50c2a50bf71a5ce44a225f9a9725c213
Ryan Chandler <ryangjchandler@gmail.com>
2023-12-01 21:42:37 +0000

    2023 Day 1: Solutions for Part 1 & 2

--- 2023/days/1.php
+++ 2023/days/1.php
@@ -2,18 +2,60 @@
 
 namespace Day1;
 
-function part1(string $input)
+function part1(string $input): int
 {
-    
+    $lines = explode(PHP_EOL, $input);
+    $total = 0;
+
+    foreach ($lines as $line) {
+        $characters = str_split($line);
+        $numericCharacters = array_values(array_filter($characters, fn (string $character) => is_numeric($character)));
+
+        $firstNumber = $numericCharacters[0];
+        $lastNumber = $numericCharacters[count($numericCharacters) - 1];
+        $result = "{$firstNumber}{$lastNumber}";
+
+        $total += $result;
+    }
+
+    return $total;
 }
 
-check('Day 1 Part 1 Example', 'inputs/day-1-part-1-example.txt', part1(...), null);
-produce('Day 1 Part 2', '2023/inputs/day-1-part-2.txt', part2(...));
+check('Day 1 Part 1 Example', '2023/inputs/day-1-part-1-example.txt', part1(...), 142);
+produce('Day 1 Part 1', '2023/inputs/day-1.txt', part1(...));
 
-function part2(string $input)
+function part2(string $input): int
 {
-    
+    $lines = explode(PHP_EOL, $input);
+    $total = 0;
+    $words = [
+        'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+    ];
+
+    foreach ($lines as $line) {
+        $characters = str_split($line);
+        $newLine = '';
+
+        foreach ($characters as $character) {
+            $newLine .= $character;
+
+            foreach ($words as $index => $word) {
+                $newLine = str_replace($word, $index + 1 . $character, $newLine);
+            }
+        }
+
+        $characters = str_split($newLine);
+        $numericCharacters = array_values(array_filter($characters, fn (string $character) => is_numeric($character)));
+
+        $firstNumber = $numericCharacters[0];
+        $lastNumber = $numericCharacters[count($numericCharacters) - 1];
+        $result = "{$firstNumber}{$lastNumber}";
+
+        $total += (int) $result;
+    }
+
+    return $total;
 }
 
-check('Day 1 Part 2 Example', 'inputs/day-1-part-2-example.txt', part2(...), null);
-produce('Day 1 Part 2', '2023/inputs/day-1-part-2.txt', part2(...));
+check('Day 1 Part 2 Example', '2023/inputs/day-1-part-2-example.txt', part2(...), 281);
+produce('Day 1 Part 2', '2023/inputs/day-1.txt', part2(...));
